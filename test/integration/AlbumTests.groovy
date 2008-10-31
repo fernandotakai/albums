26d37536a04ee2fd55e83c74794b552ea48993e3
Jeff Brown <jeff@g2one.com>
2008-10-31 14:19:20 -0500

    more album testing

--- test/integration/AlbumTests.groovy
+++ test/integration/AlbumTests.groovy
@@ -6,10 +6,10 @@ class AlbumTests extends GroovyTestCase {
 
         def titleError = album.errors?.getFieldError('title')
         assertNotNull 'title error should not have been null', titleError
-        
+
         assertTrue 'expected message not found', 'album.title.matches.error' in titleError.codes
     }
-    
+
     void testAlbumsWithSameArtistAndSameTitle() {
         def artist = new Artist(name: 'Some Artist')
         artist.save(flush: true)
@@ -19,11 +19,35 @@ class AlbumTests extends GroovyTestCase {
         artist.addToAlbums album1
         artist.save(flush: true)
         artist.addToAlbums album2
-        
+
         assertFalse 'validation should have failed', artist.validate()
 
         def albumTitleError = artist.errors.getFieldError('albums.title')
         assertNotNull 'album title error should not have been null'
         assertTrue 'expected message not found', 'album.title.unique.error' in albumTitleError.codes
+    }
+
+    void testAlbumsWithDifferentArtistAndSameTitle() {
+        def artist1 = new Artist(name: 'Some Artist')
+        artist1.save(flush: true)
+
+        def artist2 = new Artist(name: 'Some Other Artist')
+        artist2.save(flush: true)
+
+        def album1 = new Album(title: 'Some Album Title')
+        def album2 = new Album(title: 'Some Album Title')
+
+        artist1.addToAlbums album1
+        artist1.save(flush: true)
+        artist2.addToAlbums album2
+
+        artist1.validate()
+        artist2.validate()
+
+        def albumTitleError = artist1.errors.getFieldError('albums.title')
+        assertNull 'album title error should have been null', albumTitleError
+
+        albumTitleError = artist2.errors.getFieldError('albums.title')
+        assertNull 'album title error should have been null', albumTitleError
     }
 }
