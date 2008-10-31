c7fd058cc27e4a312922ef3f054cf994a961f765
Jeff Brown <jeff@g2one.com>
2008-10-31 11:07:20 -0500

    fix constraints

--- grails-app/domain/Artist.groovy
+++ grails-app/domain/Artist.groovy
@@ -1,10 +1,11 @@
 class Artist {
     String name
+    static hasMany = [albums: Album]
     
     String toString() {
         name
     }
     static constraints = {
-        name matches: /[_^]*/
+        name matches: /[^_]*/
     }
 }
