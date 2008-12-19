0a2d67e1ec498d9d0314dc033cba8c121ac9d01b
Jeff Brown <jeff@jeffandbetsy.net>
2008-12-18 19:33:02 -0600

    rework bootstrap a bit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,35 +3,30 @@ class BootStrap {
     def init = { servletContext ->
         def jeffBeck = new Artist(name: 'Jeff Beck')
         jeffBeck.save()
-        
+
         def blowByBlow = new Album(title: 'Blow By Blow')
         def wired = new Album(title: 'Wired')
 
-        def blowByBlowTracks = ['You Know What I Mean',
-        "She's A Woman",
-        'Constipated Duck',
-        'Air Blower',
-        'Scatterbrain',
-        "Cause We've Ended As Lovers",
-        'Thelonius',
-        'Freeway Jam',
-        'Diamond Dust']
+        def createTrack = { trackName -> new Track(name: trackName) }
 
-        def wiredTracks = ['Led Boots',
-        'Come Dancing',
-        'Goodbye Pork Pie Hat',
-        'Head For Backstage Pass',
-        'Blue Wind',
-        'Sophie',
-        'Play With Me',
-        'Love Is Green']
+        blowByBlow.tracks = ['You Know What I Mean',
+                             "She's A Woman",
+                             'Constipated Duck',
+                             'Air Blower',
+                             'Scatterbrain',
+                             "Cause We've Ended As Lovers",
+                             'Thelonius',
+                             'Freeway Jam',
+                             'Diamond Dust'].collect(createTrack)
 
-        def addTrack = { album, trackName ->
-            album.addToTracks name: trackName
-        }
-
-        blowByBlowTracks.each addTrack.curry(blowByBlow)
-        wiredTracks.each addTrack.curry(wired)
+        wired.tracks = ['Led Boots',
+                        'Come Dancing',
+                        'Goodbye Pork Pie Hat',
+                        'Head For Backstage Pass',
+                        'Blue Wind',
+                        'Sophie',
+                        'Play With Me',
+                        'Love Is Green'].collect(createTrack)
 
         jeffBeck.addToAlbums blowByBlow
         jeffBeck.addToAlbums wired
